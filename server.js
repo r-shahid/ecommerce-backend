0b1a1bc9ab2cbd780e92870d86116cbec334fceb
estebang625 <estebang7511@gmail.com>
2020-10-26 08:09:20 -0700

    adjusted server file

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const corsOptions = require('./configs/cors.js');
 
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
+
+// product and reviews routes:
+const productRoutes = require('./controllers/productRoutes');
+const reviewRoutes = require('./controllers/reviewsRoutes');
 
 NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 app.use(cors()) //anybody can make a request
 app.use(express.json());
+app.use(morgan("tiny"));
 
 // middleware configuration? might need this later? no idea...
 // app.use(logger('dev'));
@@ -21,11 +27,6 @@ app.get('/', (req, res) => {
 	res.json({ Welcome: 'Hello impulse shoppers!' });
 });
 
-// product and reviews routes:
- const productRoutes = require('./controllers/productRoutes');
- const reviewRoutes = require('./controllers/reviewsRoutes');
-
- 
 // app.use:
 app.use('/products', productRoutes);
 app.use('/reviews', reviewRoutes);
